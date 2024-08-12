4702486cabe8a81c90e5190e37b23e54ea78df59
Johan <Johanotr@gmail.com>
2024-08-12 14:19:55 -0400

    Add operation functions

---
+++ script/script.js
@@ -0,0 +1,20 @@
+const display = document.querySelector("#display")
+const displayText = document.querySelector("#display p")
+
+// Operation functions
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function divide(a, b){
+    return a / b;
+}
+
