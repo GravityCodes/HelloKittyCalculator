892f5972f8ca729b05976dbb2d34cea5e07124c8
Johan <Johanotr@gmail.com>
2024-08-12 18:55:29 -0400

    Add operation functions

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 const display = document.querySelector("#display")
 const displayText = document.querySelector("#display p")
+const allClear = document.querySelector("#allclear")
+
+const numButtons = document.querySelectorAll("#numbers button")
+
+let firstNum = "";
+let secondNum = 0;
 
 // Operation functions
 function add(a, b){
@@ -17,4 +23,18 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+function operate(num1,num2,func){
+    return func(num1, num2)
+}
+
+//Calculator Loop
+
+
+
+
+numButtons.forEach((num) => num.addEventListener('click', () => {
+    firstNum += String(Number(num.textContent));
+    displayText.textContent = firstNum;
+}))
 
